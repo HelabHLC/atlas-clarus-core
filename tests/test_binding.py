a81c15e2cc555e11dadd3eadcefeb96152968517
HelabHLC <norbert.m.woiwod@gmail.com>
2026-08-24 13:51:35 +0200

    ci: add evidence contract gates

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -40,6 +40,28 @@ class BindingTests(unittest.TestCase):
         self.assertEqual(result.source_atlas_row_id, 0)
         self.assertEqual(result.production_atlas_row_id, 0)
 
+    def test_active_delta_lambda_tie_uses_lower_row_id(self):
+        master = AtlasMaster.from_records(
+            [
+                {"reference": "LOWER_ID", "rgb": [10, 10, 10], "lambda_v2_nm": 500, "lambda_ee_nm": 505, "delta_lambda_nm": -5},
+                {"reference": "HIGHER_ID", "rgb": [11, 10, 10], "lambda_v2_nm": 500, "lambda_ee_nm": 495, "delta_lambda_nm": 5},
+            ]
+        )
+        result = AtlasBinder(master).bind((10, 10, 10), mode=ACTIVE_MODE)
+        self.assertEqual(result.source_atlas_row_id, 0)
+        self.assertEqual(result.production_atlas_row_id, 0)
+
+    def test_duplicate_rgb_uses_lower_row_id(self):
+        master = AtlasMaster.from_records(
+            [
+                {"reference": "H005_L090_C005", "rgb": [237, 224, 227], "lambda_v2_nm": 500, "lambda_ee_nm": 502, "delta_lambda_nm": -2},
+                {"reference": "H360_L090_C005", "rgb": [237, 224, 227], "lambda_v2_nm": 500, "lambda_ee_nm": 501, "delta_lambda_nm": -1},
+            ]
+        )
+        result = AtlasBinder(master).bind((237, 224, 227))
+        self.assertEqual(result.source_atlas_row_id, 0)
+        self.assertEqual(result.source_reference, "H005_L090_C005")
+
     def test_invalid_rgb_is_rejected(self):
         master = AtlasMaster.from_records(
             [{"reference": "ONLY", "rgb": [0, 0, 0], "lambda_v2_nm": 500, "lambda_ee_nm": 500, "delta_lambda_nm": 0}]
